6a88afa4b9707a52c4fa8a925004d56b944ed7f1
David Petrosyan <dpetrosyan@smedialink.com>
2019-12-06 11:21:45 +0300

    initial commit

---
+++ movies-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  movies-app
+//
+//  Created by David Petrosyan on 12/6/19.
+//  Copyright © 2019 David Petrosyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
